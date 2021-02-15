6dea8a67c165599db62301860e7f5d6689f8eba2
Andrew Luckett <andyluckett9@gmail.com>
2021-02-15 02:43:06 +0000

    Improve pause logic

--- Assets/Scripts/Generic/GCO_PauseController.cs
+++ Assets/Scripts/Generic/GCO_PauseController.cs
@@ -8,26 +8,42 @@ public class GCO_PauseController : MonoBehaviour {
     **/
 
     public GameObject pausewindow;
-    private bool paused = false;
+    private List<AudioSource> sounds;
 
     void Start() {
-
+        setPauseMenuActive(pausewindow.activeSelf);
     }
 
 
     void Update() {
         if(Input.GetButtonDown("Cancel")) {
-            setPauseMenuActive(!paused);
+            setPauseMenuActive(!pausewindow.activeSelf);
         }
     }
 
     public void setPauseMenuActive(bool active) {
-        paused = active;
         Time.timeScale = active ? 0f : 1f;
         pausewindow.SetActive(active);
 
         Cursor.lockState = active ? CursorLockMode.None : CursorLockMode.Locked;
         Cursor.visible = active;
-        
+
+
+        //Warning the following sound pausing may cause desync in the long run
+        if(active) {
+            AudioSource[] allsounds = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+            sounds = new List<AudioSource>();
+            foreach(AudioSource auso in allsounds) {
+                if(auso.isPlaying) {
+                    sounds.Add(auso);
+                    auso.Pause();
+                }
+            }
+        } else {
+            foreach(AudioSource auso in sounds) {
+                auso.Play();
+            }
+        }
+
     }
 }
